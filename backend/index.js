9b6553f01341853849cf0d9ff04ea9deeb7ebc71
stefa-n <67799417+stefa-n@users.noreply.github.com>
2024-05-02 23:46:56 +0300

    feat: add price

--- backend/index.js
+++ backend/index.js
@@ -31,6 +31,43 @@ app.get("/:marketplace/:product", cors(), async (req, res) => {
   res.json(data);
 });
 
+app.post("/:marketplace/:product", cors(), async (req, res) => {
+  console.log(`POST: ${req.params.marketplace}/${req.params.product}`);
+
+  if (!req.body.price) {
+    return res.status(400).json({ error: "Price is required" });
+  }
+
+  const { data: latestData, error: latestError } = await supabase
+    .from("prices")
+    .select("*")
+    .eq("marketplace", req.params.marketplace)
+    .eq("product", req.params.product)
+    .order("created_at", { ascending: false });
+
+  if (latestError) {
+    return res.status(500).json({ error: latestError.message });
+  }
+
+  if (latestData.length > 0 && latestData[0].price === req.body.price) {
+    return res.status(200).json({ message: "Price is the same" });
+  }
+
+  const { error } = await supabase.from("prices").insert([
+    {
+      marketplace: req.params.marketplace,
+      product: req.params.product,
+      price: req.body.price,
+    },
+  ]);
+
+  if (error) {
+    return res.status(500).json({ error: error.message });
+  }
+
+  res.status(200);
+});
+
 app.listen(3001, () => {
   console.log("Server is running on port 3001");
 });
