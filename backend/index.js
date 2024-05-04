b035db72482a2e04b4013e8e3587a214c588b729
stefa-n <67799417+stefa-n@users.noreply.github.com>
2024-05-04 15:54:35 +0300

    feat: custom backend port

--- backend/index.js
+++ backend/index.js
@@ -69,6 +69,6 @@ app.post("/:marketplace/:product", async (req, res) => {
   res.status(200);
 });
 
-app.listen(3001, () => {
-  console.log("Server is running on port 3001");
+app.listen(process.env.PORT, () => {
+  console.log("Server is running on port " + process.env.PORT);
 });
