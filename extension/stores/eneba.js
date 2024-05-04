444b19a5e0ccfcddea52aaeb7b737f47b59a87e1
stefa-n <67799417+stefa-n@users.noreply.github.com>
2024-05-04 15:53:01 +0300

    feat: eneba

---
+++ extension/stores/eneba.js
@@ -0,0 +1,62 @@
+// eneba does advertisments on the biggest buy button, even if it's not the cheapest
+// so we need to check which one is the cheapest and give that price to the database
+// thank you eneba, very cool >_<
+
+// also, it features a loading screen, so i have create an interval that checks if the price is loaded
+// there MIGHT be a better way to do this, but i'm not sure how to do it and i'm too lazy to look it up
+
+let found = false;
+let url = "";
+
+let interval = setInterval(() => {
+  if (url != window.location.href) {
+    found = false;
+
+    let price = document.getElementsByClassName("w7tD3i")[0];
+    let other_price = document.getElementsByClassName("L5ErLT")[0];
+
+    if (price && !found) {
+      found = true;
+      let element = document.createElement("span");
+      element.innerHTML = `<img src="http://localhost:3000/logo.png" alt="vezipret" style="width: auto; height: 80px; margin-top: -30px; margin-bottom: -31px; margin-left: -1px"/>`;
+
+      price = price.innerText;
+      price = parseFloat(
+        price.replace(/\./g, "").replace(".", "").replace(",", ".")
+      );
+
+      other_price = other_price.innerText;
+      other_price = parseFloat(
+        other_price.replace(/\./g, "").replace(".", "").replace(",", ".")
+      );
+
+      if (price > other_price) price = other_price;
+
+      let produs = document
+        .getElementsByClassName("C68dpx")[0]
+        .innerText.replace("&", "and"); // browser doesn't like & in the url
+
+      element.onclick = function () {
+        window.open(
+          "http://localhost:3000/price?marketplace=eneba&product=" + produs
+        );
+      };
+
+      document.getElementsByClassName("L5ErLT")[0].appendChild(element);
+
+      fetch(`http://localhost:3001/eneba/${produs}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          price: price,
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+        .catch((error) => console.error("Error:", error));
+      url = window.location.href;
+    } /* else {
+      console.log("no price found");
+    } */
+  }
+}, 1000);
